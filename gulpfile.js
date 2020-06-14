192b80f8f5a35829c7596252a91ba522c7307185
SaintyLiue <541491286@qq.com>
2020-06-14 09:42:27 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require("gulp");
+
+var folder = {
+  src: "./src/",
+  build: "./build/"
+};
+
+//流读取文件      task running grunt 
+gulp.task("images", function () {
+  gulp.src(folder.src + "images/*")
+    .pipe(gulp.dest(folder.build + "images"));
+});
